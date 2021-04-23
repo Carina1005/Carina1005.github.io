e76adc0207a361d6d1d37bc15fda43d9df6f192b
CarinaYuan <>
2021-04-23 03:17:58 +0000

    Revert "theme installed"
    This reverts commit 3521c367a2d419eb0701b798dd9cba6da5cc6138.

--- main.sh
+++ main.sh
@@ -1,7 +1,6 @@
-echo Please Wait...
-mkdir ../.gem
-cp -r gemBINS/* ../.gem
+echo Please Wait....
+cp -r gemBINS ../.gem
 export PATH=/home/runner/.gem/ruby/2.5.0/bin:$PATH
 jekyll -v
 cd site
-bundle exec jekyll serve -P 8080 --host=0.0.0.0
+jekyll serve -P 8080 --host=0.0.0.0
